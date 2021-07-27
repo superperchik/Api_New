9b86909d2c64f9b16b2c59e4fe07441041931b51
superperchik <60111258+superperchik@users.noreply.github.com>
2021-07-27 13:05:43 +0300

    Api

---
+++ EFCoreCodeFirstSample/Models/AuditableEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirstSample.Models
+{
+    public class AuditableEntity //: IAuditableEntity Тут наверное какой-то другой класс или создать именно его?
+    {
+        [Column("id", Order = 0)]
+        [Key]
+        public long Id { get; set; }
+
+        [Column("created_by", Order = 101)]
+        [MaxLength(256)]
+        public string CreatedBy { get; set; }
+
+        [Column("updated_by", Order = 103)]
+        [MaxLength(256)]
+        public string UpdatedBy { get; set; }
+
+        [Column("created_at", TypeName = "timestamp without time zone", Order = 102)]
+        public DateTime CreatedDate { get; set; }
+
+        [Column("updated_at", TypeName = "timestamp without time zone", Order = 104)]
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
---
+++ EFCoreCodeFirstSample/Startup.cs
@@ -0,0 +1,63 @@
+using EFCoreCodeFirstSample.Models;
+using EFCoreCodeFirstSample.Models.DataManager;
+using EFCoreCodeFirstSample.Models.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirstSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<EmployeeContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:EmployeeDB"]));
+            services.AddScoped<IDataRepository<Employee>, EmployeeManager>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
+            app.UseRouting();
+            app.UseCors("CorsPolicy");
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
