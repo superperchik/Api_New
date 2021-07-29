8e8b5baecfb20a584fb7ebf06ee97649786eea8d
superperchik <60111258+superperchik@users.noreply.github.com>
2021-07-29 23:36:33 +0300

    New settings and script

--- EFCoreCodeFirstSample/Models/AuditableEntity.cs
+++ EFCoreCodeFirstSample/Models/AuditableEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace EFCoreCodeFirstSample.Models
 {
-    public class AuditableEntity //: IAuditableEntity Тут наверное какой-то другой класс или создать именно его?
+    public class AuditableEntity //: DbContext //: IAuditableEntity Тут наверное какой-то другой класс или создать именно его?
     {
         [Column("id", Order = 0)]
         [Key]
---
+++ EFCoreCodeFirstSample/Models/DictAntibioticConfiguration.cs
@@ -0,0 +1,43 @@
+﻿/*
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirstSample.Models
+{
+    public class DictAntibioticConfiguration : IEntityTypeConfiguration<DictAntibiotic>
+    {
+        public void Configure(EntityTypeBuilder<DictAntibiotic> builder)
+        {
+            builder.ToTable("dict_antibiotics").HasKey(m => m.Id);
+            builder.ToTable("dict_antibiotics").HasIndex(x => x.Name).IsUnique();
+
+            builder
+                .Property(m => m.Id)
+                .HasColumnName("id")
+                .HasDefaultValueSql("uuid_generate_v4()");
+            builder
+                .Property(m => m.Name)
+                .HasColumnName("name")
+                .HasDefaultValue("")
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            builder
+                .Property(m => m.Created)
+                .HasColumnName("created_at")
+                .IsRequired()
+                .HasDefaultValueSql("NOW()");
+            builder
+                .Property(m => m.Updated)
+                .HasColumnName("updated_at")
+                .IsRequired()
+                .HasDefaultValueSql("NOW()");
+        }
+    }
+
+}
+*/
